3b603407c8a855f1559aff56ebfceaa99e780d02
hudiniq <simon.ffs@gmail.com>
2020-04-23 12:56:33 +0200

    -iterrows
    index and iloc

--- parser.py
+++ parser.py
@@ -15,23 +15,25 @@ for i in range(0, len(df.columns)):
 df.columns = headers
 
 #def find_start():
-for index, row in df.iterrows():
-    if row[0] == "COD. ARTICOLO / ITEM CODE": 
+for index in df.index:
+    if df.iloc[index, 0] == "COD. ARTICOLO / ITEM CODE": 
         start = index + 1
 
 #def find_orders():
-for index, row in islice(df.iterrows(), start, None):
+for index in islice(df.index, start, None):
+# for index in islice(df.iterrows(), start, None):
     try:
-        int(row[0])
-        for index2, row2 in islice(df.iterrows(), index, None):
-            if row2[0] == "UN":
+        int(df.iloc[index, 0])
+        # int(row[0])
+        for index2 in islice(df.index, index, None):
+            if df.iloc[index2, 0] == "UN":
                 order_end = index2
                 break
         orders_position.append((index, order_end - 1))
     except ValueError:
         pass
 
-#def fill_orders():
+# #def fill_orders():
 for order, order_end in orders_position:
 
     code_row = df.iloc[order_end]
@@ -42,12 +44,12 @@ for order, order_end in orders_position:
         except ValueError:
             pass
 
-    for index, row in islice(df.iterrows(), order + 1, order_end):
+    for index in islice(df.index, order + 1, order_end):
         item = {
             "supplier_code" : df.iloc[order, 0],
-            "lotto" : row[5],
-            "opis" : row[12],
-            "kolicina" : int(row[9] / 1000),
+            "lotto" : df.iloc[index, 5],
+            "opis" : df.iloc[index, 12],
+            "kolicina" : int(df.iloc[index, 9] / 1000),
             "code" : code,
         }
         orders.append(item)
